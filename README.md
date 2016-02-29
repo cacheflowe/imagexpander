569cfcbde206538ad0f0684359c07773c12c98f2
cacheflowe <cacheflowe@cacheflowe.com>
2016-02-28 21:19:41 -0700

    Use width/height instead of scale. Add large-src.

--- README.md
+++ README.md
@@ -6,11 +6,11 @@
 
 ## Usage
 
-Simply add the .js and .css files to your project, and add an `imagexpander` class to any image you'd like to expand on click. When an image is expanded, a subsequent click, window scroll or ESC key press will hide it.
+Simply add the .js and .css files to your poject, and add an `imagexpander` class to any image you'd like to expand on click. When an image is expanded, a subsequent click, window scroll or ESC key press will hide it. You can also add the attribute `data-imagexpander-large-src` to your image element to load a higher-res image once expanded.
 
 If you'd rather cover the screen instead of letterbox, use the public method `window.imagexpander.setStyleCrop()`
 
-If you'd like to change the screen padding, use the public method `window.imagexpander.setPadding(1)`, where `1` sets no padding, and `0.9` fills 90% of the screen.
+If you'd like to change the screen padding, use the public method `window.imagexpander.setPadding(1)`, where `1` sets no padding, and `0.9` fills 90% of the screen in letterbox style.
 
 ## Demo
 
--- imagexpander.js
+++ imagexpander.js
@@ -25,20 +25,57 @@
 
   // calculate size & position
   var expandImage = function(img) {
+    // grab img & calc size
     _expandedImg = img;
     var imgBounds = _expandedImg.getBoundingClientRect();
     var containerW = window.innerWidth * _padding;
     var containerH = window.innerHeight * _padding;
     var ratio = getRatioToCrop(containerW, containerH, imgBounds.width, imgBounds.height, _crops);
-    var targetTop = (window.innerHeight - imgBounds.height) / 2; // top of image if it was centered in the page
-    var imgY = targetTop - imgBounds.top;
-    var targetLeft = (window.innerWidth - imgBounds.width) / 2; // left of image if it was centered in the page
-    var imgX = targetLeft - imgBounds.left;
-    _expandedImg.style[transformProp] = 'translate3d(' + imgX + 'px,' + imgY + 'px,0) scale(' + ratio + ')';
-    _expandedImg.classList.add('imagexpander-showing');
-    window.addEventListener('scroll', shrinkImage);
-    window.addEventListener('resize', shrinkImage);
-    document.addEventListener('keydown', checkEscClose);
+    var newW = Math.round(_expandedImg.width * ratio);
+    var newH = Math.round(_expandedImg.height * ratio);
+
+    // set current size manually to aid with animation
+    _expandedImg.style.width = imgBounds.width + 'px';
+    _expandedImg.style.maxWidth = imgBounds.width + 'px';
+    _expandedImg.style.height = imgBounds.height + 'px';
+    _expandedImg.setAttribute('data-shrink-to', imgBounds.width+','+imgBounds.height);
+
+    // add wrapper div to hold height if needed
+    if(_expandedImg.parentNode.classList.contains('imagexpander-wrapper') == false) {
+      var wrapper = document.createElement('div');
+      wrapper.className = 'imagexpander-wrapper';
+      _expandedImg.parentNode.insertBefore(wrapper, _expandedImg);
+      wrapper.appendChild(_expandedImg);
+    }
+
+    // set parent's height to stick where it is
+    _expandedImg.parentNode.style.height = _expandedImg.parentNode.getBoundingClientRect().height + 'px';
+    setTimeout(function(){
+      // calc position. reset to 0,0 by negative offsets, then position from there
+      var imgX = Math.round(-imgBounds.left + (window.innerWidth - newW) / 2);
+      var imgY = Math.round(-imgBounds.top + (window.innerHeight - newH) / 2);
+
+      // set css
+      document.body.style.overflowX = 'hidden';
+      _expandedImg.style[transformProp] = 'translate3d(' + imgX + 'px,' + imgY + 'px,0)';
+      _expandedImg.style.width = newW + 'px';
+      _expandedImg.style.maxWidth = newW + 'px';
+      _expandedImg.style.height = newH + 'px';
+      _expandedImg.classList.add('imagexpander-showing');
+
+      // add listeners to close
+      window.addEventListener('scroll', shrinkImage);
+      window.addEventListener('resize', shrinkImage);
+      document.addEventListener('keydown', checkEscClose);
+
+      // add high res src if specified
+      setTimeout(function(){
+        if(img.getAttribute('data-imagexpander-large-src') != null) {
+          img.src = img.getAttribute('data-imagexpander-large-src');
+        }
+      }, 320);
+
+    }, 50);
   };
 
   // crop/letterbox helper
@@ -53,8 +90,22 @@
   // shrink it
   var shrinkImage = function() {
     if(_expandedImg) {
+      var shrinkSize = _expandedImg.getAttribute('data-shrink-to').split(',');
       _expandedImg.classList.remove('imagexpander-showing');
       _expandedImg.style[transformProp] = null;
+      _expandedImg.style.width = parseInt(shrinkSize[0]) + 'px';
+      _expandedImg.style.maxWidth = parseInt(shrinkSize[0]) + 'px';
+      _expandedImg.style.height = parseInt(shrinkSize[1]) + 'px';
+      console.log('shrinkTo: ', parseInt(shrinkSize[0]), parseInt(shrinkSize[1]));
+      var expandedImg = _expandedImg;
+      setTimeout(function(){
+        expandedImg.style.width = null;
+        expandedImg.style.maxWidth = null;
+        expandedImg.style.height = null;
+        expandedImg.parentNode.style.height = null;
+        expandedImg.removeAttribute('data-shrink-to')
+        document.body.style.overflowX = null;
+      }, 320);
       window.removeEventListener('scroll', shrinkImage);
       window.removeEventListener('resize', shrinkImage);
       document.removeEventListener('keypress', checkEscClose);
